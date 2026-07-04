e6500f2c61d33ddfc71cbae3e62b82a12674e0c3
仰望星空 <16939348+njsmmz@user.noreply.gitee.com>
2026-07-04 15:03:45 +0800

    清理项目，只保留网页预览版

--- _cleanup.ps1
+++
@@ -1,8 +0,0 @@
-# Clean up garbled files first
-$soundDir = "c:\Users\Administrator\Desktop\baizaoyin\shengyin"
-Get-ChildItem $soundDir -Filter "*.mp3" | ForEach-Object {
-    Remove-Item $_.FullName -Force
-    Write-Host "Deleted: $($_.Name)"
-}
-Write-Host "Cleaned up."
-
